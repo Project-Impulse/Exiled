a3771e9033e97d6945a9d22dbf213c760f785e27
Project-Impulse <impulseproject16@gmail.com>
2017-06-21 18:49:26 +0530

    Update exiled.js

--- chat-plugins/exiled.js
+++ chat-plugins/exiled.js
@@ -291,13 +291,6 @@ exports.commands = {
 	sota: function (room, user) {
 		return this.parse('feelssota feelstini tinitini sotalove');
 	},
-
-	devs: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-		this.sendReplyBox('<div style="background-color: red ; border: pink solid 2px ; height: 100px"><center><img style="transform: scaleX(-1);" src="http://pldh.net/media/pokemon/gen5/blackwhite_animated_front/491.gif" height="84" width="95" align="left"><img src="http://i.imgur.com/PgQAAI1.png" height="74" width="250"><img src="http://pldh.net/media/pokemon/gen5/blackwhite_animated_front/491.gif" height="84" width="95" align="right"></center></div><table style="text-align: center ; background-color: Black ; border: Red solid 2px ; width: 100% ; border-collapse: collapse"><tbody><tr><td style="border: Red solid 2px ; color: White ; width: 22%"><img style="transform: scaleX(-1);" src="https://avatars2.githubusercontent.com/u/20971990?v=3&s=460" height="80" width="80"><br>Insist</td><td style="border: Red solid 2px ; color: White ; width: 22%"><img src="http://i.imgur.com/C3bFaZT.png" height="80" width="80"><br>Ninetales >n<</td><td style="border: Red solid 2px ; color: White ; width: 22%"><img style="transform: scaleX(-1);" src="https://files.graphiq.com/620/media/images/Volcanion_5208962.png" height="80" width="80"><br>Volco</td><td style="border: Red solid 2px ; color: White ; width: 22%"><img src="http://i.imgur.com/IXS2qYX.png" height="80" width="80"><br>HoeenHero</td></tr></tbody></table>');
-	},
-	devshelp: ["/devs - Shows the coders of the server."],
-
 	'!define': true,
 	def: 'define',
 	define: function (target, room, user) {
@@ -643,7 +636,7 @@ exports.commands = {
 		saveReports();
 		Rooms('staff').add('A new report has been submitted by ' + user.name + '. ID: ' + reportId + ' Message: ' + target.trim());
 		Rooms('staff').update();
-		return this.sendReply("Your report has been sent to Exiled global authorities..");
+		return this.sendReply("Your report has been sent to Impulse global authorities..");
 	},
 
 	reports: function (target, room, user, connection, cmd) {
@@ -837,32 +830,6 @@ exports.commands = {
 	},
 	backhelp: ["/back - Sets a users away status back to normal."],
 
-	'!essb': true,
-	essb: function (target, room, user) {
-		if (!this.runBroadcast()) return false;
-		if (!target || target === 'help') return this.parse('/help essb');
-		if (target === 'credits') return this.parse('/essbcredits');
-		let targetData = getMonData(toId(target));
-		if (!targetData) return this.errorReply("The staffmon '" + toId(target) + "' could not be found.");
-		return this.sendReplyBox(targetData);
-	},
-
-	essbhelp: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-		return this.sendReplyBox("/essb [staff member\'s name] - displays data for a staffmon\'s movepool, custom move, and custom ability.");
-	},
-
-	essbcredits: function (target, room, user) {
-		let popup = "|html|" + "<font size=5 color=#000080><u><b>ESSB Credits</b></u></font><br />" +
-			"<br />" +
-			"<u><b>Programmers:</u></b><br />" +
-			"- " + Exiled.nameColor('Insist', true) + " (Head Developer, Idea, Balancer, Concepts, Entries.)<br />" +
-			"- " + Exiled.nameColor('VXN', true) + " (Assistant Developer)<br />" +
-			"- " + Exiled.nameColor('Back At My Day', true) + " (Entries, Developments.)<br />" +
-			"<u><b>Special Thanks:</b></u><br />" +
-			"- Our Staff Members for their cooperation in making this.<br />";
-		user.popup(popup);
-	},
 	'!dub': true,
 	dub: 'dubtrack',
 	music: 'dubtrack',
